3fa0f733ec6ad49f5d09495ad981b89973280d04
BoratBorat10 <110756460+BoratBorat10@users.noreply.github.com>
2023-01-22 11:34:21 +0200

    Update leafv2.js
    added . to line 2 and 3

--- leafv2.js
+++ leafv2.js
@@ -1,6 +1,6 @@
 import LatLon, { Dms } from 'https://cdn.jsdelivr.net/npm/geodesy@2/latlon-spherical.js';
-import noUiSlider from '/nouislider/dist/nouislider.js'
-import {geotrack} from '/geo_track.js'
+import noUiSlider from './nouislider/dist/nouislider.js'
+import {geotrack} from './geo_track.js'
 
 var map = L.map('map',{
     center: [32.03993,34.82497],
