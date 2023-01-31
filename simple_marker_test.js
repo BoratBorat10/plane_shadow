d64f512cbc8b25d6931854ad1e61eee72062274e
BoratBorat10 <110756460+BoratBorat10@users.noreply.github.com>
2023-01-31 23:15:17 +0200

    got the shadow line working
    but in draws in multiple times

--- simple_marker_test.js
+++ simple_marker_test.js
@@ -15,33 +15,69 @@ var slider = document.getElementById("slider");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
  
-var marker = L.marker([32.07,34.78]).addTo(map)
-const ele =500
 
-var shadow_test = new LatLon(32.07,34.78).destinationPoint(1500,300);
-var shadowtestmarker = L.marker([shadow_test._lat,shadow_test._lon]).addTo(map);
-console.log(shadow_test._lat,shadow_test._lon)
+//marker testing
+var marker = L.marker([32.067627,34.764091]).addTo(map)
+const ele =670
 
 
+//create plane array
+const plane_ar = []
+for (var i in geotrack){
+    plane_ar.push({'lat': geotrack[i].properties.lon, 'lng': geotrack[i].properties.lat, 'ele': geotrack[i].properties.ele})   
+}
+//create a line fomr the array
+var planeline = L.polyline(plane_ar,{color:'red'}).addTo(map)
+console.log(plane_ar)
+
+var shadow_ar = [] //create empty array for shadows
 
 
 
+//just shaodw marker to be set.latlon later
 var shadow_marker = L.marker([32.07,34.78]).addTo(map)
 
-var date = new Date()
-console.log(shadow_marker)
+var date = new Date() //takes curent date
+
 //chnages the date with the slider
 document.getElementById('slider').addEventListener('input', function (){
     var value = this.value
     
     date.setHours(this.value)
-
-
+    // the slider supplies 0-23 number and this corresoponds to a hour of the day that is re set
 });
-slider.oninput = function() {
-    output.innerHTML = this.value;
-  } 
 
+//chnages the value diaplay under the slider
+slider.oninput = function() {
+    output.innerHTML = this.value;} 
+
+
+//function that takes a point and reterns its shadow point
+function shadowcalc(start_array,end_array){
+for (var i in start_array){
+
+    var sunpos= SunCalc.getPosition(date,start_array[i].lat,start_array[i].lng)
+
+    var sunazi =  ((sunpos.azimuth * (180.0/Math.PI))) //0 is south so 180 needs to be added- took me hours to get this
+    var sunalt =  (sunpos.altitude * 180.0/Math.PI)
+
+    var shadowlen = (start_array[i].ele / Math.tan(sunpos.altitude))
+
+    var endpoint = new LatLon(start_array[i].lat,start_array[i].lng).destinationPoint(shadowlen,(sunazi))
+
+    end_array.push({'lat': endpoint._lat, 'lng': endpoint._lon, 'shadowlen': shadowlen})
+    
+
+}
+
+}
+
+
+console.log(plane_ar[3].ele)
+
+var shadowline = L.polyline(shadow_ar,{color:'black'}).addTo(map)
+
+//when the slider changes:
 document.getElementById('slider').addEventListener('input', function (){
 var sunpos= SunCalc.getPosition(date,32.07,34.78)
 
@@ -51,15 +87,23 @@ var sunalt =  (sunpos.altitude * 180.0/Math.PI)
 var shadowlen = (ele / Math.tan(sunpos.altitude))
 
 //var startpoint = new LatLon(32.07,34.78)
-var endpoint = new LatLon(32.07,34.78).destinationPoint(shadowlen,(sunazi+360))
+var endpoint = new LatLon(32.067627,34.764091).destinationPoint(shadowlen,(sunazi+360))
+
+
+shadowcalc(plane_ar,shadow_ar)
+
+console.log(shadow_ar[0])
+//console.log(shadow_ar)
+
+
 
 
 
 
 shadow_marker.setLatLng([endpoint._lat, endpoint._lon])
-console.log(date)
-//var line = L.polyline([32.07,34.78],[shadow_pos._lon,shadow_pos._lat],{color:'black'}).addTo(map)
 
+var shadowline = L.polyline(shadow_ar,{color:'black'}).addTo(map)
+console.log(plane_ar)
 
 
 const para = document.getElementById("info")
@@ -69,3 +113,4 @@ const para = document.getElementById("info")
 
 
 });
+
